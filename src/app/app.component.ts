20880cc7ded76a05f4c4e3015261eaa0098efa49
peesaeung <peeranut_saeung@hotmail.com>
2021-07-13 13:51:14 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
-@Component({
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+@Component ({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  constructor(private httpClient: HttpClient) { }
   title = 's2q2';
+  f = new FormControl('');
+  users: Array<any>;
+  uf: Array<any>;
+  ngOnInit(): void {
+    this.getAllUsers(); // this is called on page load
+  }
+  getAllUsers() {
+    this.httpClient.get('https://api.publicapis.org/categories')
+      .subscribe(
+        (response: Array<any>) => {
+            this.users = response;
+          },
+        error => {
+            console.log(error);
+          }
+      );
+  }
+  filtered(e) {
+    console.log(e.target.value);
+    this.uf = this.users.filter((i) => i.includes(this.f.value));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,7 +9,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
